37638d513e7f9b5790a60aa47d808355c688e86e
Vincent "MooZ" Cruz <mooz@blockos.org>
2014-07-09 23:19:14 +0200

    Moved demos and added separate cmake files.

--- CMakeModules/FindAntTweakBar.cmake
+++ CMakeModules/FindAntTweakBar.cmake
@@ -3,43 +3,68 @@
 # Once done this will define
 #
 # ANT_TWEAK_BAR_FOUND
-# ANT_TWEAK_BAR_INCLUDE_PATH
-# ANT_TWEAK_BAR_LIBRARY
+# ANT_TWEAK_BAR_INCLUDE_PATH/ANT_TWEAK_BAR_INCLUDE_DIR
+# ANT_TWEAK_BAR_LIBRARIES/ANT_TWEAK_BAR_LIBRARY
 # 
 
-IF (WIN32)
-	FIND_PATH( ANT_TWEAK_BAR_INCLUDE_PATH AntTweakBar.h
-      PATHS
-		$ENV{ANT_TWEAK_BAR_ROOT}/include
-		DOC "The directory where AntTweakBar.h resides")
+if (ANT_TWEAK_BAR_LIBRARIES AND ANT_TWEAK_BAR_INCLUDE_PATH)
+  # in cache already
+  set(ANT_TWEAK_BAR_FOUND TRUE)
+else (ANT_TWEAK_BAR_LIBRARIES AND ANT_TWEAK_BAR_INCLUDE_PATH)
 
-    FIND_LIBRARY( ANT_TWEAK_BAR_LIBRARY AntTweakBar
+if(WIN32)
+    find_path(ANT_TWEAK_BAR_INCLUDE_DIR
+        NAMES AntTweakBar.h
         PATHS
-        $ENV{ANT_TWEAK_BAR_ROOT}/lib
-        DOC "The AntTweakBar library")
-ELSE (WIN32)
-    FIND_PATH(ANT_TWEAK_BAR_INCLUDE_PATH AntTweakBar.h
-      PATHS
-      /usr/local/include
-      /usr/X11/include
-      /usr/include)
+            $ENV{ANT_TWEAK_BAR_ROOT}/include
+        DOC
+            "The directory where AntTweakBar.h resides"
+    )
 
-FIND_LIBRARY( ANT_TWEAK_BAR_LIBRARY AntTweakBar
-  PATHS
-    /usr/local
-    /usr/X11
-    /usr
-  PATH_SUFFIXES
-    lib64
-    lib
-    dylib
-)
-ENDIF (WIN32)
+    find_library(ANT_TWEAK_BAR_LIBRARY
+        AntTweakBar
+        PATHS
+            $ENV{ANT_TWEAK_BAR_ROOT}/lib
+        DOC
+            "The AntTweakBar library"
+    )
+else (WIN32)
+    find_path(ANT_TWEAK_BAR_INCLUDE_DIR
+        NAMES AntTweakBar.h
+        PATH_SUFFIXES include Include include/AntTweakBar
+        PATHS
+            /usr
+            /usr/local
+            /sw # Fink
+            /opt/local # DarwinPorts
+            /opt/csw # Blastwave
+            /opt
+    )
 
-SET(ANT_TWEAK_BAR_FOUND "NO")
-IF (ANT_TWEAK_BAR_INCLUDE_PATH AND ANT_TWEAK_BAR_LIBRARY)
-	SET(ANT_TWEAK_BAR_FOUND "YES")
-ENDIF (ANT_TWEAK_BAR_INCLUDE_PATH AND ANT_TWEAK_BAR_LIBRARY)
+    find_library(ANT_TWEAK_BAR_LIBRARY
+        NAMES AntTweakBar
+        PATH_SUFFIXES lib lib64 Library dylib
+        PATHS
+            /usr
+            /usr/local
+            /sw # Fink
+            /opt/local # DarwinPorts
+            /opt/csw # Blastwave
+            /opt
+    )
+endif (WIN32)
 
-INCLUDE(${CMAKE_ROOT}/Modules/FindPackageHandleStandardArgs.cmake)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(AntTweakBar DEFAULT_MSG ANT_TWEAK_BAR_INCLUDE_PATH ANT_TWEAK_BAR_LIBRARY)
+if (ANT_TWEAK_BAR_LIBRARY)
+  set(ANT_TWEAK_BAR_LIBRARIES ${ANT_TWEAK_BAR_LIBRARIES} ${ANT_TWEAK_BAR_LIBRARY})
+endif (ANT_TWEAK_BAR_LIBRARY)
+
+if (ANT_TWEAK_BAR_DIR)
+  set(ANT_TWEAK_BAR_INCLUDE_PATH ${ANT_TWEAK_BAR_PATH} ${ANT_TWEAK_BAR_DIR})
+endif (ANT_TWEAK_BAR_DIR)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(ANT_TWEAK_BAR DEFAULT_MSG ANT_TWEAK_BAR_LIBRARY ANT_TWEAK_BAR_INCLUDE_DIR)
+
+mark_as_advanced(ANT_TWEAK_BAR_INCLUDE_DIR ANT_TWEAK_BAR_INCLUDE_PATH ANT_TWEAK_BAR_LIBRARY ANT_TWEAK_BAR_LIBRARIES)
+
+endif (ANT_TWEAK_BAR_LIBRARIES AND ANT_TWEAK_BAR_INCLUDE_PATH)
